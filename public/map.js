2eb94748cae1c89b345241134173fd0e62d53bb6
xSquez <ardaveahmet1@gmail.com>
2023-02-10 23:27:38 +0300

    Add Pointer

--- public/map.js
+++ public/map.js
@@ -57,6 +57,17 @@ function eqfeed_callback(results) {
       map: map,
     });
   }
+  if (dropdownValue == 'pointer') {
+    for (let i = 0; i < results.features.length; i++) {
+      const coords = results.features[i].geometry.coordinates;
+      const latLng = new google.maps.LatLng(coords[1], coords[0]);
+  
+      new google.maps.Marker({
+        position: latLng,
+        map: map,
+      });
+    }
+  }
 }
 
 window.initMap = initMap;
